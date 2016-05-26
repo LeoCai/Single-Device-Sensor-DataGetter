f7b64f76fc04ae10365fcf79af92cccf606e3569
342624757@qq.com <342624757@qq.com>
2016-05-26 23:21:25 +0800

    mobile data getter

---
+++ sensordatagetter/src/main/java/com/leocai/sensordatagetter/PublicConstants.java
@@ -0,0 +1,21 @@
+package com.leocai.sensordatagetter;
+
+import java.util.UUID;
+
+/**
+ * Created by leocai on 16-1-5.
+ */
+public class PublicConstants {
+    public static final long SENSOPR_PERIOD = 20;
+    public static final int SHAKING_DATA_SIZE = 50;
+//    public static final UUID WEAR_UUID_INSECURE = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a96");
+    public static final String NAME_WEAR_DATA = "BluetoothChatWear";
+    public static final String NAME_WEAR_DATA2 = "BluetoothChatWear2";
+    public static final UUID WEAR_UUID_INSECURE = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a96");
+    public static final String addressMI = "7C:1D:D9:7D:56:B8";
+    public static final String addressSum = "B8:C6:8E:B5:A3:06";
+    public static final String MASTER_ADDRESS = addressMI;
+    public static final String addressSumNote = "B8:B4:C8:8A:01:C7";
+    public static final String  addressMeiZu = "22:22:9A:F5:63:F1";
+
+}
